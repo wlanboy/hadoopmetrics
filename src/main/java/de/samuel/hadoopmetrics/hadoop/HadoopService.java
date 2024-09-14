0d356980769d7c195c86a22eb13bd604eeec6eba
wlanboy <wlanboy@github.com>
2024-09-14 08:36:30 +0200

    added uri parsing

--- src/main/java/de/samuel/hadoopmetrics/hadoop/HadoopService.java
+++ src/main/java/de/samuel/hadoopmetrics/hadoop/HadoopService.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import io.micrometer.core.instrument.MeterRegistry;
@@ -11,6 +12,8 @@ import io.micrometer.core.instrument.MeterRegistry;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Service
 public class HadoopService {
@@ -23,19 +26,25 @@ public class HadoopService {
 
     private Map<String, Long> directorySizes = new HashMap<>();
 
-    public void updateDirectorySizes(String directoryPath) throws IOException {
+    @Scheduled(fixedRate = 3600000) //every hour
+    public void updateMetrics() throws IOException, URISyntaxException {
+        updateDirectorySizes("/");
+        generatePrometheusMetrics();
+    }
+
+    public void updateDirectorySizes(String directoryPath) throws IOException, URISyntaxException {
         FileStatus[] fileStatuses = fileSystem.listStatus(new Path(directoryPath));
         for (FileStatus status : fileStatuses) {
             if (status.isDirectory()) {
-                directorySizes.put(status.getPath().toString(), status.getLen());
+                URI uri = new URI(status.getPath().toString());
+                directorySizes.put(uri.getPath(), status.getLen());
             }
         }
-        generatePrometheusMetrics();
     }
 
     private void generatePrometheusMetrics() {
         directorySizes.forEach((path, size) -> 
-            meterRegistry.gauge("hadoop.directory.size." + Map.of("path", path), size)
+            meterRegistry.gauge("hadoop.directory.size." + path, size)
         );
     }
 
