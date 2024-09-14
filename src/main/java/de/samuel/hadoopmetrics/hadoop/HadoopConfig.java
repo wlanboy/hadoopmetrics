3fc9d8e307b6804a4bb31ce66ad208f3f62ebc22
wlanboy <wlanboy@github.com>
2024-09-14 08:39:24 +0200

    config parameter for hfs

--- src/main/java/de/samuel/hadoopmetrics/hadoop/HadoopConfig.java
+++ src/main/java/de/samuel/hadoopmetrics/hadoop/HadoopConfig.java
@@ -1,6 +1,7 @@
 package de.samuel.hadoopmetrics.hadoop;
 
 import org.apache.hadoop.fs.FileSystem;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +11,14 @@ import java.net.URI;
 @Configuration
 public class HadoopConfig {
 
+    @Value("${hadoop.defaultFS}")
+    private String defaultFS;
+    
     @Bean
     public FileSystem fileSystem() throws IOException {
         org.apache.hadoop.conf.Configuration configuration = new org.apache.hadoop.conf.Configuration();
-        configuration.set("fs.defaultFS", "hdfs://127.0.0.1:8020");
-        return FileSystem.get(URI.create("hdfs://127.0.0.1:8020"), configuration);
+        configuration.set("fs.defaultFS", defaultFS);
+        return FileSystem.get(URI.create(defaultFS), configuration);
     }
     
 }
