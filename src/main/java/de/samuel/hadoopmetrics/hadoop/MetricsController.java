8ef57fea928e353326803b2d998a5c31e03fdddc
wlanboy <wlanboy@github.com>
2024-09-14 08:36:42 +0200

    added rest endpoint

---
+++ src/main/java/de/samuel/hadoopmetrics/hadoop/MetricsController.java
@@ -0,0 +1,25 @@
+package de.samuel.hadoopmetrics.hadoop;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MetricsController {
+    @Autowired
+    private HadoopService hadoopService;
+
+    @GetMapping("/metricsstart")
+    public String metrics() {
+        try {
+            hadoopService.updateDirectorySizes("/");
+            hadoopService.updateMetrics();
+        } catch (IOException | URISyntaxException e) {
+            return "Metrics error" + e.getMessage();
+        }
+        return "Metrics updated";
+    }
+}
